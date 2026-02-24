18b9a65996e60bda6121dbe591efe610af778d1c
MauroCasarin <70527971+MauroCasarin@users.noreply.github.com>
2026-02-24 10:52:43 -0300

    m

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  ...tseslint.configs.recommended,
+  {
+    ignores: ["dist", "eslint.config.js"],
+  }
+];
